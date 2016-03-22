147217e285038710fe36fa89b6ce615e08252688
Rocky Chen <jinweizsu@gmail.com>
2016-03-22 15:30:41 +0800

    change pipelines.py

--- main_project/main_project/pipelines.py
+++ main_project/main_project/pipelines.py
@@ -5,7 +5,18 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
+import codecs
+from scrapy import signals
 
 class MainProjectPipeline(object):
+    def __init__(self):
+        self.file=codecs.open("tencent.json",'w',encoding='utf-8')
+
     def process_item(self, item, spider):
-        return item
+        line=json.dumps(dict(item),ensure_ascii=False)+'\n'
+	self.file.write(line)
+	return item
+	
+    def spider_closed(self,spider)
+	self.file.close()
